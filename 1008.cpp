da2db7a264fa53e919a0c30df87e0ca2c459907f
Nirob Hasan <nirob427417@gmail.com>
2023-02-06 18:50:52 +0600

    basic problem beecrowd

---
+++ 1008.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    float c;
+    cin >> a >> b >> c;
+    float d;
+    d = b * c;
+    cout << "NUMBER = " << a << endl;
+    cout << fixed << " SALARY = U$" << d << endl;
+    return 0;
+}
